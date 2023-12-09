46e82c9b582998df109780e4451f9e02f2f2bbee
Phitron Batch 3 <rahatkhanpathan99@gmail.com>
2023-12-09 18:31:19 +0600

    vercel update

--- src/components/AddService/AddService.js
+++ src/components/AddService/AddService.js
@@ -3,188 +3,200 @@ import Modal from "../Modal/Modal";
 import "./AddService.css";
 
 const AddService = () => {
-  const nameRef = useRef(null);
-  const priceRef = useRef(null);
-  const descriptionRef = useRef(null);
-  const detailsRef = useRef(null);
-  const daysRef = useRef(null);
-  const nightsRef = useRef(null);
-  const imageRef = useRef(null);
-  const detailsImageRef = useRef(null);
-  // Change Title
-  useEffect(() => {
-    document.title = "Admin Panel | Add Offer";
-    window.scrollTo(0, 0);
-  }, []);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const data = {
-      name: nameRef.current.value,
-      detailsImage: detailsImageRef.current.value,
-      image: imageRef.current.value,
-      price: priceRef.current.value,
-      description: descriptionRef.current.value,
-      details: detailsRef.current.value,
-      days: daysRef.current.value,
-      nights: nightsRef.current.value,
+    const nameRef = useRef(null);
+    const priceRef = useRef(null);
+    const descriptionRef = useRef(null);
+    const detailsRef = useRef(null);
+    const daysRef = useRef(null);
+    const nightsRef = useRef(null);
+    const imageRef = useRef(null);
+    const detailsImageRef = useRef(null);
+    // Change Title
+    useEffect(() => {
+        document.title = "Admin Panel | Add Offer";
+        window.scrollTo(0, 0);
+    }, []);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const data = {
+            name: nameRef.current.value,
+            detailsImage: detailsImageRef.current.value,
+            image: imageRef.current.value,
+            price: priceRef.current.value,
+            description: descriptionRef.current.value,
+            details: detailsRef.current.value,
+            days: daysRef.current.value,
+            nights: nightsRef.current.value,
+        };
+        fetch("https://travel-guru-server-rho-two.vercel.app/offers", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((res) => res.json())
+            .then((data) => added());
     };
-    fetch("https://fast-gorge-50252.herokuapp.com/offers", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((res) => res.json())
-      .then((data) => added());
-  };
-  const added = () => {
-    document.getElementById("modal-btn").click();
-    setTimeout(() => {
-      document.getElementById("modal-close").click();
-      nameRef.current.value = "";
-      detailsImageRef.current.value = "";
-      imageRef.current.value = "";
-      priceRef.current.value = "";
-      descriptionRef.current.value = "";
-      detailsRef.current.value = "";
-      daysRef.current.value = "";
-      nightsRef.current.value = "";
-    }, 2000);
-  };
-  return (
-    <div className="container add-service-container">
-      <Modal text={'Offer Added'}></Modal>
-      <h1 className="booking-header">Add Offer</h1>
-      <hr />
-      <div className="limiter book-limiter">
-        <div className="container-login100 align-items-start py-0">
-          <div className="wrap-login100">
-            <form
-              className="login100-form validate-form"
-              onSubmit={handleSubmit}
-            >
-              {/* Input Destination Name  */}
-              <div className="wrap-input100 validate-input">
-                <span className="label-input100 p-0">Name</span>
-                <input
-                  required
-                  ref={nameRef}
-                  className="input100 book-input"
-                  type="text"
-                  name="name"
-                  placeholder="Enter destination name"
-                />
-                <span className="focus-input100 fa"></span>
-              </div>
-              {/* Input Price  */}
-              <div className="wrap-input100 validate-input">
-                <span className="label-input100 p-0">Price</span>
-                <input
-                  required
-                  ref={priceRef}
-                  className="input100 book-input"
-                  type="number"
-                  name="price"
-                  placeholder="Enter offer price"
-                />
-                <span className="focus-input100 fa"></span>
-              </div>
-              {/* Input Image Url  */}
-              <div className="wrap-input100 validate-input">
-                <span className="label-input100 p-0">Image url</span>
-                <input
-                  required
-                  ref={imageRef}
-                  className="input100 book-input"
-                  type="text"
-                  name="imageUrl"
-                  placeholder="Enter image url"
-                />
-                <span className="focus-input100 fa"></span>
-              </div>
-              {/* Input Details Image  */}
-              <div className="wrap-input100 validate-input">
-                <span className="label-input100 p-0">Second Image Url</span>
-                <input
-                  required
-                  ref={detailsImageRef}
-                  className="input100 book-input"
-                  type="text"
-                  name="detailsImage"
-                  placeholder="Enter second image url"
-                />
-                <span className="focus-input100 fa"></span>
-              </div>
-              {/* Input Description */}
-              <div className="wrap-input100 validate-input">
-                <span className="label-input100 p-0">Description</span>
-                <textarea
-                  required
-                  ref={descriptionRef}
-                  className="input100 book-address book-input"
-                  type="text"
-                  name="description"
-                  placeholder="Enter place description"
-                />
-                <span className="focus-input100 fa"></span>
-              </div>
-              {/* Input Details */}
-              <div className="wrap-input100 validate-input">
-                <span className="label-input100 p-0">Details</span>
-                <textarea
-                  required
-                  ref={detailsRef}
-                  className="input100 book-input book-address"
-                  type="text"
-                  name="details"
-                  placeholder="Enter tour details"
-                />
-                <span className="focus-input100 fa"></span>
-              </div>
-              {/* Input Tour Duration */}
-              <div className="d-flex align-items-end">
-                <div className="wrap-input100 validate-input">
-                  <span className="label-input100 p-0">Duration</span>
-                  <input
-                    required
-                    ref={daysRef}
-                    className="input100 book-input book-address"
-                    type="number"
-                    name="days"
-                    placeholder="Enter days"
-                  />
-                  <span className="focus-input100 fa"></span>
-                </div>
-                <div className="wrap-input100 validate-input">
-                  <input
-                    required
-                    ref={nightsRef}
-                    className="input100 book-input book-address"
-                    type="number"
-                    name="nights"
-                    placeholder="Enter nights"
-                  />
-                  <span className="focus-input100 fa"></span>
-                </div>
-              </div>
+    const added = () => {
+        document.getElementById("modal-btn").click();
+        setTimeout(() => {
+            document.getElementById("modal-close").click();
+            nameRef.current.value = "";
+            detailsImageRef.current.value = "";
+            imageRef.current.value = "";
+            priceRef.current.value = "";
+            descriptionRef.current.value = "";
+            detailsRef.current.value = "";
+            daysRef.current.value = "";
+            nightsRef.current.value = "";
+        }, 2000);
+    };
+    return (
+        <div className="container add-service-container">
+            <Modal text={"Offer Added"}></Modal>
+            <h1 className="booking-header">Add Offer</h1>
+            <hr />
+            <div className="limiter book-limiter">
+                <div className="container-login100 align-items-start py-0">
+                    <div className="wrap-login100">
+                        <form
+                            className="login100-form validate-form"
+                            onSubmit={handleSubmit}
+                        >
+                            {/* Input Destination Name  */}
+                            <div className="wrap-input100 validate-input">
+                                <span className="label-input100 p-0">Name</span>
+                                <input
+                                    required
+                                    ref={nameRef}
+                                    className="input100 book-input"
+                                    type="text"
+                                    name="name"
+                                    placeholder="Enter destination name"
+                                />
+                                <span className="focus-input100 fa"></span>
+                            </div>
+                            {/* Input Price  */}
+                            <div className="wrap-input100 validate-input">
+                                <span className="label-input100 p-0">
+                                    Price
+                                </span>
+                                <input
+                                    required
+                                    ref={priceRef}
+                                    className="input100 book-input"
+                                    type="number"
+                                    name="price"
+                                    placeholder="Enter offer price"
+                                />
+                                <span className="focus-input100 fa"></span>
+                            </div>
+                            {/* Input Image Url  */}
+                            <div className="wrap-input100 validate-input">
+                                <span className="label-input100 p-0">
+                                    Image url
+                                </span>
+                                <input
+                                    required
+                                    ref={imageRef}
+                                    className="input100 book-input"
+                                    type="text"
+                                    name="imageUrl"
+                                    placeholder="Enter image url"
+                                />
+                                <span className="focus-input100 fa"></span>
+                            </div>
+                            {/* Input Details Image  */}
+                            <div className="wrap-input100 validate-input">
+                                <span className="label-input100 p-0">
+                                    Second Image Url
+                                </span>
+                                <input
+                                    required
+                                    ref={detailsImageRef}
+                                    className="input100 book-input"
+                                    type="text"
+                                    name="detailsImage"
+                                    placeholder="Enter second image url"
+                                />
+                                <span className="focus-input100 fa"></span>
+                            </div>
+                            {/* Input Description */}
+                            <div className="wrap-input100 validate-input">
+                                <span className="label-input100 p-0">
+                                    Description
+                                </span>
+                                <textarea
+                                    required
+                                    ref={descriptionRef}
+                                    className="input100 book-address book-input"
+                                    type="text"
+                                    name="description"
+                                    placeholder="Enter place description"
+                                />
+                                <span className="focus-input100 fa"></span>
+                            </div>
+                            {/* Input Details */}
+                            <div className="wrap-input100 validate-input">
+                                <span className="label-input100 p-0">
+                                    Details
+                                </span>
+                                <textarea
+                                    required
+                                    ref={detailsRef}
+                                    className="input100 book-input book-address"
+                                    type="text"
+                                    name="details"
+                                    placeholder="Enter tour details"
+                                />
+                                <span className="focus-input100 fa"></span>
+                            </div>
+                            {/* Input Tour Duration */}
+                            <div className="d-flex align-items-end">
+                                <div className="wrap-input100 validate-input">
+                                    <span className="label-input100 p-0">
+                                        Duration
+                                    </span>
+                                    <input
+                                        required
+                                        ref={daysRef}
+                                        className="input100 book-input book-address"
+                                        type="number"
+                                        name="days"
+                                        placeholder="Enter days"
+                                    />
+                                    <span className="focus-input100 fa"></span>
+                                </div>
+                                <div className="wrap-input100 validate-input">
+                                    <input
+                                        required
+                                        ref={nightsRef}
+                                        className="input100 book-input book-address"
+                                        type="number"
+                                        name="nights"
+                                        placeholder="Enter nights"
+                                    />
+                                    <span className="focus-input100 fa"></span>
+                                </div>
+                            </div>
 
-              <div className="container-login100-form-btn mt-4">
-                <div className="wrap-login100-form-btn">
-                  <div className="login100-form-bgbtn"></div>
-                  <input
-                    type="submit"
-                    value="ADD OFFER"
-                    className="login100-form-btn book-btn"
-                  />
+                            <div className="container-login100-form-btn mt-4">
+                                <div className="wrap-login100-form-btn">
+                                    <div className="login100-form-bgbtn"></div>
+                                    <input
+                                        type="submit"
+                                        value="ADD OFFER"
+                                        className="login100-form-btn book-btn"
+                                    />
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-              </div>
-            </form>
-          </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default AddService;
--- src/components/BookOffer/BookOffer.js
+++ src/components/BookOffer/BookOffer.js
@@ -5,70 +5,74 @@ import Modal from "../Modal/Modal";
 import "./BookOffer.css";
 
 const BookOffer = () => {
-  const id = useParams().id;
-  const [data, setData] = useState({});
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    document.title = "Travel Guru | Book Offer";
-    window.scrollTo(0, 0);
-    fetch(`https://fast-gorge-50252.herokuapp.com/offers/${id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      });
-  }, []);
-  const added = () => {
-    document.getElementById("modal-btn").click();
-    setTimeout(() => {
-      document.getElementById("modal-close").click();
-    }, 2000);
-  };
-  return (
-    <div className="container book-container">
-      <Modal text={"Booked Successfully"}></Modal>
-      {loading ? (
-        <div className="spinner d-flex align-items-center justify-content-center">
-          <button className="btn btn-primary" type="button" disabled>
-            <span
-              className="spinner-border spinner-border-sm"
-              role="status"
-              aria-hidden="true"
-            ></span>
-            <span className="ms-2">Loading...</span>
-          </button>
+    const id = useParams().id;
+    const [data, setData] = useState({});
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        document.title = "Travel Guru | Book Offer";
+        window.scrollTo(0, 0);
+        fetch(`https://travel-guru-server-rho-two.vercel.app/offers/${id}`)
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data);
+                setLoading(false);
+            });
+    }, []);
+    const added = () => {
+        document.getElementById("modal-btn").click();
+        setTimeout(() => {
+            document.getElementById("modal-close").click();
+        }, 2000);
+    };
+    return (
+        <div className="container book-container">
+            <Modal text={"Booked Successfully"}></Modal>
+            {loading ? (
+                <div className="spinner d-flex align-items-center justify-content-center">
+                    <button className="btn btn-primary" type="button" disabled>
+                        <span
+                            className="spinner-border spinner-border-sm"
+                            role="status"
+                            aria-hidden="true"
+                        ></span>
+                        <span className="ms-2">Loading...</span>
+                    </button>
+                </div>
+            ) : (
+                <>
+                    <div className="tour-details">
+                        <h1 className="name">{data.name}</h1>
+                        <hr />
+                        <div className="d-flex justify-content-between">
+                            <p className="card-text duration">
+                                {" "}
+                                <i className="fas fa-clock"></i> {data.days}{" "}
+                                DAYS - {data.nights} NIGHTS{" "}
+                            </p>
+                            <p className="price">${data.price}</p>
+                        </div>
+                    </div>
+                    <div className="row gy-5">
+                        <div className="col-md-7">
+                            <img
+                                src={data.detailsImage}
+                                className="w-100 book-img"
+                                alt=""
+                            />
+                            <div className="details-div mt-3">
+                                <p className="details">Details</p>
+                                <hr />
+                                <p className="book-details">{data.details}</p>
+                            </div>
+                        </div>
+                        <div className="col-md-5 form-container">
+                            <Form data={data} added={added}></Form>
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
-      ) : (
-        <>
-          <div className="tour-details">
-            <h1 className="name">{data.name}</h1>
-            <hr />
-            <div className="d-flex justify-content-between">
-              <p className="card-text duration">
-                {" "}
-                <i className="fas fa-clock"></i> {data.days} DAYS -{" "}
-                {data.nights} NIGHTS{" "}
-              </p>
-              <p className="price">${data.price}</p>
-            </div>
-          </div>
-          <div className="row gy-5">
-            <div className="col-md-7">
-              <img src={data.detailsImage} className="w-100 book-img" alt="" />
-              <div className="details-div mt-3">
-                <p className="details">Details</p>
-                <hr />
-                <p className="book-details">{data.details}</p>
-              </div>
-            </div>
-            <div className="col-md-5 form-container">
-              <Form data={data} added={added}></Form>
-            </div>
-          </div>
-        </>
-      )}
-    </div>
-  );
+    );
 };
 
 export default BookOffer;
